4e2d6bbd4de2c4be327a8d4aa53f199c2d740abd
Anthony O'Brien <aux.fuse@yahoo.com>
2022-02-19 21:50:40 +0000

    Function added to handle element creation in main.js, code refactored

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,14 @@
 const communityContainer = document.querySelector('#community');
 
+createElement = (element, className) => {
+    // Simple function for defining Elements and class names of same
+    const el = document.createElement(element);
+    el.className = className;
+    return el;
+};
+
 fetch("community.json")
+    // Fetch JSON data & add to DOM
     .then(
         response => response.json()
     )
@@ -15,22 +23,26 @@ fetch("community.json")
             ]);
 
             participants.forEach((participant) => {
-                const card = document.createElement("div");
-                card.className = "card";
-                const pName = document.createElement("h2");
-                const pCourseStart = document.createElement("p");
-                const pCourseStage = document.createElement("p");
-                const pFavLanguage = document.createElement("p");
-                const pCurrentLearn = document.createElement("p");
+                // Create Elements
+                const card = createElement("div", "card");
+                const name = createElement("h2", "card-title");
+                const courseStart = createElement("p", "card-sub");
+                const courseStage = createElement("p", "card-sub");
+                const favLanguage = createElement("p", "card-detail");
+                const currentLearn = createElement("p", "card-detail");
+                
+                // Inject data to new Created elements
+                name.innerHTML = participant[0];
+                courseStart.innerHTML = participant[1];
+                courseStage.innerHTML = participant[2];
+                favLanguage.innerHTML = participant[3];
+                currentLearn.innerHTML = participant[4];
 
-                pName.innerHTML += participant[0];
-                pCourseStart.innerHTML += participant[1];
-                pCourseStage.innerHTML += participant[2];
-                pFavLanguage.innerHTML += participant[3];
-                pCurrentLearn.innerHTML += participant[4];
-
+                // Hierarchy of Container
                 communityContainer.append(card);
-                card.append(pName, pCourseStart, pCourseStage, pFavLanguage, pCurrentLearn);
+                card.append(
+                    name, courseStart, courseStage, favLanguage, currentLearn
+                );
             })
         }
     );
