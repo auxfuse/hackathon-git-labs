c966b666d13039ba41ddcc14337cf3e8aaf47e5a
Anthony O'Brien <aux.fuse@yahoo.com>
2022-02-19 13:47:40 +0000

    map values of JSON objects to participants array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-community = {};
-
 const communityContainer = document.querySelector('#community');
 
 fetch("community.json")
@@ -8,25 +6,12 @@ fetch("community.json")
     )
     .then(
         data => {
-            data.participants.forEach((dev) => {
-                let participant = {
-                    name: dev.name,
-                    course_stage: dev.course_stage,
-                    course_start: dev.course_start,
-                    favorite_language: dev.favorite_language,
-                    currently_learning: dev.currently_learning
-                };
-
-                const {
-                    name,
-                    course_stage,
-                    course_start,
-                    favorite_language,
-                    currently_learning
-                } = participant;
-
-                communityContainer.innerHTML = name + " " + course_stage + " " + course_start + " " + favorite_language + " " + currently_learning;
-            })
+            let participants = data.map(participant => [
+                participant.name,
+                participant.course_start,
+                participant.course_stage,
+                participant.favorite_language,
+                participant.currently_learning
+            ]);
         }
     );
-
