2987fe0f346d25a7559b3e0bd7b1f0365e7eca6a
Anthony O'Brien <aux.fuse@yahoo.com>
2022-02-19 15:22:10 +0000

    updated main.js file to print data to html file and updated flex css for index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,5 +13,24 @@ fetch("community.json")
                 participant.favorite_language,
                 participant.currently_learning
             ]);
+
+            participants.forEach((participant) => {
+                const card = document.createElement("div");
+                card.className = "card";
+                const pName = document.createElement("h2");
+                const pCourseStart = document.createElement("p");
+                const pCourseStage = document.createElement("p");
+                const pFavLanguage = document.createElement("p");
+                const pCurrentLearn = document.createElement("p");
+
+                pName.innerHTML += participant[0];
+                pCourseStart.innerHTML += participant[1];
+                pCourseStage.innerHTML += participant[2];
+                pFavLanguage.innerHTML += participant[3];
+                pCurrentLearn.innerHTML += participant[4];
+
+                communityContainer.append(card);
+                card.append(pName, pCourseStart, pCourseStage, pFavLanguage, pCurrentLearn);
+            })
         }
     );
