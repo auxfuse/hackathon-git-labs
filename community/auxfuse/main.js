0c632a52629a6d6f12f4a9f62517d58d9d4a9322
Anthony O'Brien <aux.fuse@yahoo.com>
2022-09-18 23:04:23 +0100

    Testing JS removal on production site

--- community/auxfuse/main.js
+++ community/auxfuse/main.js
@@ -1,15 +1,15 @@
-// Global variables
-let touch_device = (navigator.maxTouchPoints || 'ontouchstart' in document.documentElement);
-let bgSVG = document.querySelector('#svg-bg');
+// // Global variables
+// let touch_device = (navigator.maxTouchPoints || 'ontouchstart' in document.documentElement);
+// let bgSVG = document.querySelector('#svg-bg');
 
-/**
- * If browser is "Mobile" variant, remove ColorMatrix Element
- * @param {boolean} touch_device - checks for current device using TouchPoints or "ontouchstart" event method
-*/
-function detectDevice(touch_device) {
-    if(touch_device) {
-        bgSVG.remove();
-    }
-};
+// /**
+//  * If browser is "Mobile" variant, remove ColorMatrix Element
+//  * @param {boolean} touch_device - checks for current device using TouchPoints or "ontouchstart" event method
+// */
+// function detectDevice(touch_device) {
+//     if(touch_device) {
+//         bgSVG.remove();
+//     }
+// };
 
-window.onload = detectDevice(touch_device);
+// window.onload = detectDevice(touch_device);
