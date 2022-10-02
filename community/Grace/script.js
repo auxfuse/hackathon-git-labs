d645893760f8a53e00de50855d53451c7477df00
gracemcken <grace.ellen.mckenna@gmail.com>
2022-10-02 16:43:56 +0100

    Add functions to open links for socials in JS

--- community/Grace/script.js
+++ community/Grace/script.js
@@ -1,1 +1,26 @@
 // To call modal when resume button is clicked
+
+// To open links when buttons are clicked
+// Twitter
+const twitter = document.getElementById("twitter");
+twitter.addEventListener("click", () => {
+  window.open("https://twitter.com/msgracie_mac", "_blank");
+});
+
+// Instagram
+const instagram = document.getElementById("instagram");
+instagram.addEventListener("click", () => {
+  window.open("https://www.instagram.com/miss_gracieface/", "_blank");
+});
+
+// LinkedIn
+const linkedin = document.getElementById("linkedin");
+linkedin.addEventListener("click", () => {
+  window.open("https://www.linkedin.com/in/grace-mckenna-bb7066111/", "_blank");
+});
+
+// GitHub
+const github = document.getElementById("github");
+github.addEventListener("click", () => {
+  window.open("https://github.com/gracemcken", "_blank");
+});
