154de3a925c2188caaf96cf2dc09f81475afc4d2
seanyoung247 <seanyoung247@gmail.com>
2022-10-04 22:36:41 +0100

    feat: Adds showcase creation

--- assets/js/showcase.js
+++ assets/js/showcase.js
@@ -1,3 +1,4 @@
+
 const generateCard = (participant, template) => {
     // Create a new card element based on the template
     const card = template.content.firstElementChild.cloneNode(true);
@@ -9,6 +10,23 @@ const generateCard = (participant, template) => {
         }
     }
     return card;
+}
+
+/**
+ * Creates and appends 
+ * @param {Array} participants - Array of participant objects 
+ * @param {Element} template - The template element object to build individual cards from
+ * @param {Element} container - The dom element to appened the cards too
+ * @param {Number} showcaseCount - The number of showcases to create
+ */
+const createShowcases = (participants, template, container, showcaseCount) => {
+    const fragment = new DocumentFragment();
+    showcaseCount = Math.min(participants.length, showcaseCount);
+
+    for (let i = 0; i < showcaseCount; i++) {
+        fragment.append(generateCard(participants[i], template));
+    }
+    container.append(fragment);
 }
 (()=>{
     let showcaseCount = 5;
