7ce710967dbb0594bb7c87c13f6e5edacbf1a662
seanyoung247 <seanyoung247@gmail.com>
2022-10-04 22:36:15 +0100

    feat: Adds card generation

--- assets/js/showcase.js
+++ assets/js/showcase.js
@@ -1,3 +1,15 @@
+const generateCard = (participant, template) => {
+    // Create a new card element based on the template
+    const card = template.content.firstElementChild.cloneNode(true);
+    for (const child of card.children) {
+        if (child.dataset.id == 'link') {
+            child.setAttribute('href', `community/${participant.name}`);
+        } else {
+            child.children[0].innerText = participant[child.dataset.id];
+        }
+    }
+    return card;
+}
 (()=>{
     let showcaseCount = 5;
 })();
