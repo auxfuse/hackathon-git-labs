a64c9e49f3f4ab598cb65aff4926d99570e396dd
seanyoung247 <seanyoung247@gmail.com>
2022-10-05 11:07:49 +0100

    refactor: Moves showcase code to main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,8 @@ const getRandomEmoji = async () => {
     .then((data) => data[Math.floor(Math.random() * data.length)]);
 };
 
+
+
 /**
  * It takes an array of objects, l
  * oops through each object, and creates a card for each object
@@ -36,6 +38,75 @@ const createParticipantes = (listOfParticipants) => {
   });
 };
 
-fetch("assets/data/community.json")
-  .then((response) => response.json())
-  .then((data) => createParticipantes(data));
+const generateCardFromTemplate = (participant, template) => {
+  const card = template.content.firstElementChild.cloneNode(true);
+  const fields = card.querySelectorAll('[data-field]');
+
+  for (const field of fields) {
+      if (field.dataset.field == 'link') {
+          field.setAttribute('href', `community/${participant.name}`);
+      } else {
+          field.children[0].innerText = participant[field.dataset.field];
+      }
+  }
+  return card;
+}
+
+/**
+* Creates and appends showcase cards 
+* @param {Array} participants - Array of participant objects 
+* @param {Array} indexes - List of random indexes of participants
+* @param {Array} pages - Array of participants with custom pages
+* @param {Object} showcases - Object of showcase data
+*/
+const createShowcases = (participants, indexes, pages, showcases) => {
+  const fragment = new DocumentFragment();
+  let remaining = showcases.count;
+
+  for (const idx of indexes) {
+      // Check participant has custom page
+      if (pages.includes(participants[idx].name)) {
+          remaining--;
+          fragment.append(generateCardFromTemplate(participants[idx], showcases.template))
+      }
+      if (!remaining) break;
+  }
+
+  showcases.container.append(fragment);
+}
+
+const shuffle = (arr) => {
+  for(let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i+1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+const genRandomIndexes = (max, min = 0) => {
+  const indexes = new Array(max - min).fill(0).map((v,i)=>i+min);
+  return shuffle(indexes);
+}
+
+(()=>{
+    const showcases = {
+        count: 5,
+        container: document.getElementById("showcases"),
+        template: document.querySelector("#showcases > .item-template")
+    };
+
+    // Preload data
+    Promise.all([
+        fetch("assets/data/community.json")
+            .then(response => response.json()),
+        fetch("assets/data/communitypages.json")
+            .then(response => response.json()),
+    ]).then((values) => {
+        // When all data has loaded:
+        if (showcases.container && showcases.template) {
+            createShowcases(values[0], genRandomIndexes(values[0].length), values[1], showcases);
+        }
+        createParticipantes(values[0]);
+    });
+
+})();
