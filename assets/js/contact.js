7fda35bb92409b504cdb1a79fcd8b5fe0044f493
Alissa <alissatroianonyc@gmail.com>
2022-10-07 10:31:28 -0400

    Use jsnoify to display user input in JSON

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,10 +1,22 @@
+// function handleUserInput(data) {
+
+// }
+
 var inputName = document.getElementById('name_input');
 var inputEmail = document.getElementById('email_input')
+var queryType = document.getElementById('category');
 
 inputName.onkeyup = function(){
-    document.getElementById('print_name').innerHTML = `{ ` + `<br>` + `     name: ` + inputName.value + `,`;
+    document.getElementById('print_name').innerHTML = JSON.stringify(inputName.value);
 }
 
 inputEmail.onkeyup = function(){
-    document.getElementById('print_email').innerHTML =  `     email: ` + inputEmail.value + `,`;
+    document.getElementById('print_email').innerHTML = JSON.stringify(inputEmail.value);
 }
+
+queryType.onselect = function(){
+    for (category in categories) {
+        document.getElementById('print_query').innerHTML = JSON.stringify(queryType.value);
+    }
+}
+
