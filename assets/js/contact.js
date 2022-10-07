dd00b4a60ddccb80389f7a03435ded18444ba661
Alissa <alissatroianonyc@gmail.com>
2022-10-07 11:11:19 -0400

    Add final functionality for JSON display

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,17 +1,28 @@
-var inputName = document.getElementById('name_input');
-var inputEmail = document.getElementById('email_input')
-var queryType = document.getElementById('category');
+var inputName = document.getElementById("name_input");
+var inputEmail = document.getElementById("email_input");
+var queryType = document.getElementById("category");
+var inputMessage = document.getElementById("message_input");
 
-inputName.onkeyup = function(){
-    document.getElementById('print_name').innerHTML = JSON.stringify(inputName.value);
-}
+inputName.onkeyup = function () {
+  document.getElementById("print_name").innerHTML = JSON.stringify(
+    inputName.value
+  );
+};
 
-inputEmail.onkeyup = function(){
-    document.getElementById('print_email').innerHTML = JSON.stringify(inputEmail.value);
-}
+inputEmail.onkeyup = function () {
+  document.getElementById("print_email").innerHTML = JSON.stringify(
+    inputEmail.value
+  );
+};
 
 function showQueryType() {
-    selElement = document.querySelector('#category');
-    option = selElement.value;
-    document.querySelector('.print_query').textContent = option;
+  selElement = document.querySelector("#category");
+  option = selElement.value;
+  document.querySelector(".print_query").textContent = option;
 }
+
+inputMessage.onkeyup = function () {
+  document.getElementById("print_message").innerHTML = JSON.stringify(
+    inputMessage.value
+  );
+};
