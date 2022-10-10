0f4922bc0d9462cbff7a21a3deb1a672d0bda6e9
Alissa <alissatroianonyc@gmail.com>
2022-10-10 11:23:44 -0400

    Add fixed bottom navigation and grids

---
+++ community/Alissa2/assets/js/skills.js
@@ -0,0 +1,12 @@
+
+const control = document.getElementById("direction-toggle");
+const marquees = document.querySelectorAll(".marquee");
+const wrapper = document.querySelector(".wrapper");
+
+control.addEventListener("click", () => {
+  control.classList.toggle("toggle--vertical");
+  wrapper.classList.toggle("wrapper--vertical");
+  [...marquees].forEach((marquee) =>
+    marquee.classList.toggle("marquee--vertical")
+  );
+});
