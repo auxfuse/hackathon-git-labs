997254923463c085c38874d280723bb4726161f5
Alissa <alissatroianonyc@gmail.com>
2022-10-10 11:35:58 -0400

    Delete extra files

--- community/Alissa2/assets/js/nav.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("hamburgerNav").style.top = "0";
-  } else {
-    document.getElementById("hamburgerNav").style.top = "-50px";
-  }
-}
