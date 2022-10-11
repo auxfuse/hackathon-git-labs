d60750369f88e53dd116cc6b1fac1c26df3e2e35
Sean Young <seanyoung247@gmail.com>
2022-10-11 22:04:05 +0100

    feat: Adds slideshow scroll capture code

--- assets/js/modules/slideshow.js
+++ assets/js/modules/slideshow.js
@@ -1,22 +1,40 @@
 
 export const setupSlideshow = (rootEl) => {
 
-    const content = rootEl.querySelector('.slideshow-content > iframe');
+    const content = rootEl.querySelector('.slideshow-contents');
+    const details = rootEl.querySelector('.slideshow-details');
     const prevBtn = rootEl.querySelector('.slideshow-control.prev');
     const nextBtn = rootEl.querySelector('.slideshow-control.next');
-    
-    // Prevents click events within the iframe
-    content.contentDocument.body.style.pointerEvents = 'none';
-    content.addEventListener('load', (e) => {
-        e.target.contentDocument.body.style.pointerEvents = 'none';
+
+    // Capture scroll event and pass it to the iframe
+    details.addEventListener('wheel', e => {
+        const scrollPos = content.contentWindow.scrollY + (e.deltaY * 2);
+        const scrollBottom = content.contentDocument.body.scrollHeight 
+                                - content.contentWindow.innerHeight;
+
+        content.contentWindow.scrollTo({
+            top: scrollPos,
+            left: 0,
+            behavior: 'smooth'
+        });
+
+        // Do we want to prevent the outer page from scrolling?
+        if (scrollPos > 0 && scrollPos < scrollBottom) {
+            e.preventDefault();
+        }
+    });
+
+    details.addEventListener('click', e => {
+        console.log('details clicked');
     });
 
     // Attach forward/back button events
     prevBtn.addEventListener('click', e => {
-
+        console.log('back clicked');
     });
     
     nextBtn.addEventListener('click', e => {
-
+        console.log('next clicked');
     });
+
 };
