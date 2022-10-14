1bd4bc91b47b8122ca642d72b4090d38c7c3b6be
nikimacm <nickymacm@gmail.com>
2022-10-14 15:48:53 +0000

    updated colour scheme and styling

--- community/Nicola/script.js
+++ community/Nicola/script.js
@@ -1,3 +1,10 @@
 $('.card').click(function(){
     $(this).toggleClass('flipped');
   });
+
+  jQuery('.x-face-button').each(function() {
+    var theHref = jQuery(this).attr('href');
+    jQuery(this).closest('.x-face-outer.back').on('click', function() {
+      window.location.href= theHref;
+    });
+  });
