6c3db57b37c22565b13a25dc51dae4ca7723b9ab
Sean Young <seanyoung247@gmail.com>
2022-10-14 13:06:46 +0100

    refactor: Makes slideshow class anonymous

--- assets/js/components/slideshow.js
+++ assets/js/components/slideshow.js
@@ -50,33 +50,35 @@
                     </svg>
                 </slot>
             </button>
-   
+
         </div>
     `;
-    
-        static get observedAttributes() {
-            return ['timeout'];
+   
+    customElements.define('slide-show', 
+        class extends HTMLElement {
+            static get observedAttributes() {
+                return ['timeout'];
+            }
+
+            constructor() {
+                super();
+                this.shadow = this.attachShadow({mode: 'open'});
+                this._timeout = 0;
+            }
+
+            attributeChangedCallback(property, oldValue, newValue) {
+                if (oldValue === newValue) return;
+                this[property] = newValue;
+            }
+
+            // TODO: Reflect parameter change back to DOM attibutes.
+            get timeout() {return this._timeout;}
+            set timeout(val) {this._timeout = val;}
+
+            connectedCallback() {
+                this.shadow.append(template.content.cloneNode(true));
+                console.log(this._timeout);
+            }
         }
-
-        constructor() {
-            super();
-            this.shadow = this.attachShadow({mode: 'open'});
-            this._timeout = 10;
-        }
-
-        attributeChangedCallback(property, oldValue, newValue) {
-            if (oldValue === newValue) return;
-            this[property] = newValue;
-        }
-
-        get timeout() {return this._timeout;}
-        set timeout(val) {this._timeout = val;}
-
-        connectedCallback() {
-            this.shadow.append(template.content.cloneNode(true));
-            console.log(this._timeout);
-        }
-    }
-
-    customElements.define('slide-show', SlideShow);
+    );
 })();
