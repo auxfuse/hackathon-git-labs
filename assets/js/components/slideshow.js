8dd01b5794d30cdbb4a234e69de079f00cfe0491
Sean Young <seanyoung247@gmail.com>
2022-10-14 13:06:18 +0100

    feat: Adds html/css slideshow template

--- assets/js/components/slideshow.js
+++ assets/js/components/slideshow.js
@@ -2,9 +2,57 @@
 (() => {
 
     const template = document.createElement("template");
-    template.innerHTML = '<h2>Template code</h2>';
+    template.innerHTML = `
+        <style>
+            * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+            #slide-controls {
+                display: flex;
+                justify-content: space-between;
+                width: 100%;
+                height: 100%;
+            }
+            .slide-btn {
+                width: 50px;
+                background: transparent;
+                border: none;
+                opacity: 0.5;
+                transition: opacity 0.5s;
+            }
+            .slide-btn:hover {
+                opacity: 0.75;
+            }
+            .control-icon {
+                fill: white;
+                stroke: black;
+            }
+        </style>
+
+        <slot></slot>
+
+        <div id="slide-controls">
+
+            <button id="prev" class="slide-btn">
+                <slot name="previous-slide-icon">
+                    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 10 10'>
+                        <path class="control-icon" d="m6.2851 1.0156 1.4142 1.4142-2.5702 2.5702 2.5702 2.5702-1.4142 1.4142-3.9844-3.9844z"/>
+                    </svg>
+                </slot>
+            </button>
+
+            <button id="next" class="slide-btn">
+                <slot name="next-slide-icon">
+                    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 10 10'>
+                        <path class="control-icon" d="m2.3007 2.4298 1.4142-1.4142 3.9844 3.9844-3.9844 3.9844-1.4142-1.4142 2.5702-2.5702z"/>
+                    </svg>
+                </slot>
+            </button>
    
-    class SlideShow extends HTMLElement {
+        </div>
+    `;
     
         static get observedAttributes() {
             return ['timeout'];
