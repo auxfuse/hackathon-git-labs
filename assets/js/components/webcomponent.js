179c2752f97d7eace87c461e915753138a533256
Sean Young <seanyoung247@gmail.com>
2022-10-15 13:57:44 +0100

    feat: Initial Web component base class

---
+++ assets/js/components/webcomponent.js
@@ -0,0 +1,27 @@
+
+/**
+ * A class for simplyfing webcomponent creation. 
+ * Handles much of the boiler plate and component creation code.
+ */
+export class WebComponent extends HTMLElement {
+
+    static attributes = {}
+
+    static get observedAttributes() {
+        return [];
+    }
+
+
+    constructor() {
+        super();
+    }
+
+
+    attributeChangedCallback(property, oldValue, newValue) {
+        if (oldValue === newValue) return;
+        // Attributes are always strings, so decode it to the correct datatype
+    }
+
+};
+
+// Component compilation code goes below, yo.
