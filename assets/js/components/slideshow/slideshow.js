eef75ff18d9d936dfcf84c42acefac448b3dc700
Sean Young <seanyoung247@gmail.com>
2022-10-17 22:28:33 +0100

    feat: Improves slide change code

--- assets/js/components/slideshow/slideshow.js
+++ assets/js/components/slideshow/slideshow.js
@@ -2,8 +2,6 @@ import { WebComponent, loadTemplate } from '../webcomponent.js';
 import { wrapInRange } from '../../modules/utilities.js';
 
 (() => {
-    const templateUrl = new URL('slideshow.html', import.meta.url).href;
-
     class SlideShow extends HTMLElement {
         static template = null;
 
@@ -25,13 +23,15 @@ import { wrapInRange } from '../../modules/utilities.js';
         constructor() {
             super();
             this.attachShadow({mode: 'open'});
-
+            this.shadowRoot.append(this.constructor.template.content.cloneNode(true));
+            // Elements
             this._slides = null;
             this._nextBtn = null;
             this._prevBtn = null;
-
+            // Properties
             this._timeout = 0;
-            this._currentSlide = 0;
+            this._animation = 'none';
+            this._slide = 0;
         }
 
         attributeChangedCallback(property, oldValue, newValue) {
@@ -45,51 +45,41 @@ import { wrapInRange } from '../../modules/utilities.js';
             this.setAttribute('timeout', val);
         }
 
+        get slide() { return this._slide; }
+        set slide(val) {
+            if (this.slideCount > 0) {
+                // Hide currently displayed slide
+                this._slides.assignedElements()[this._slide].classList.remove('show-slide');
+                // Ensure index is within range
+                this._slide = wrapInRange(0, val, this.slideCount - 1);
+                // Show the current selected slide
+                this._slides.assignedElements()[this._slide].classList.add('show-slide');
+                // Reflect the property to the element attribute
+                this.setAttribute('slide', this._slide);
+            }
+        }
+
         connectedCallback() {
-            this.shadowRoot.append(template.content.cloneNode(true));
             this._slides = this.shadowRoot.querySelector('#slides');
             this._prevBtn = this.shadowRoot.getElementById('prev');
             this._nextBtn = this.shadowRoot.getElementById('next');
 
             // Try to show the intial slide
-            this.showSlide(0);
+            this.slide = 0;
 
             // Watch for slides being added or removed
-            this._slides.addEventListener('slotchange', this._onSlidesChange.bind(this));
+            this._slides.addEventListener('slotchange', () => this.slide = this.slide);
 
             // Setup slideshow control events
-            this._prevBtn.addEventListener('click', this.prevSlide.bind(this));
-            this._nextBtn.addEventListener('click', this.nextSlide.bind(this));
+            this._prevBtn.addEventListener('click', () => this.slide--);
+            this._nextBtn.addEventListener('click', () => this.slide++);
         }
 
-        slideCount() {
+        get slideCount() {
+            if (!this._slides) return 0;
             return this._slides.assignedElements().length;
         }
-
-        showSlide(which) {
-            if (this._slides.assignedElements().length > 0) {
-                // Hide currently displayed slide
-                this._slides.assignedElements()[this._currentSlide].classList.remove('show-slide');
-                // Ensure index is within range
-                which = wrapInRange(0, which, this.slideCount() - 1);
-                // Show the current selected slide
-                this._slides.assignedElements()[which].classList.add('show-slide');
-            }
-            return which;
-        }
-
-        prevSlide() {
-            this._currentSlide = this.showSlide(this._currentSlide - 1);
-        }
-
-        nextSlide() {
-            this._currentSlide = this.showSlide(this._currentSlide + 1);
-        }
-
-        _onSlidesChange(e) {
-            this._currentSlide = this.showSlide(this._currentSlide);
-        }
-    }
+    };
 
     const templateUrl = new URL('slideshow.html', import.meta.url).href;
     loadTemplate(templateUrl)
