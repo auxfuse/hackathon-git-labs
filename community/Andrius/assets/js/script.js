3a5c7ded119bf88863f31b974c1c0c01f73528a2
andrius-siup <andrius.siupinys@gmail.com>
2022-10-18 12:38:06 +0100

    Implemented modal functionality

--- community/Andrius/assets/js/script.js
+++ community/Andrius/assets/js/script.js
@@ -1,6 +1,9 @@
 const btns = document.querySelectorAll(".tab-btn");
 const about = document.querySelector(".about");
 const articles = document.querySelectorAll(".content");
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
 
 about.addEventListener("click", function (e) {
   // console.log(e.target.dataset.id);
@@ -19,4 +22,12 @@ about.addEventListener("click", function (e) {
     const element = document.getElementById(id);
     element.classList.add("active");
   }
+});
+
+modalBtn.addEventListener("click", function () {
+  modal.classList.add("open-modal");
+});
+
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
 });
