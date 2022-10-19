af31080aedcac76dc9b0156b56ad40defae59f75
andrius-siup <andrius.siupinys@gmail.com>
2022-10-19 19:05:24 +0100

    Implemented contact me functionality - have to add your keys

--- community/Andrius/assets/js/script.js
+++ community/Andrius/assets/js/script.js
@@ -31,3 +31,27 @@ modalBtn.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
   modal.classList.remove("open-modal");
 });
+
+function SendMail() {
+  const params = {
+    // Send values
+    from_name: document.getElementById("userName").value,
+    email_id: document.getElementById("email_id").value,
+    message: document.getElementById("message").value,
+    // new
+    phone: document.getElementById("phone").value,
+    budget: document.getElementById("budget").value,
+  };
+  emailjs.send("service_id", "template_id", params).then(function (res) {
+    // alert("Success! " + res.status);
+    console.log("Success " + res.status);
+    // Then clear all values
+    document.getElementById("userName").value = "";
+    document.getElementById("email_id").value = "";
+    document.getElementById("message").value = "";
+    // new
+    document.getElementById("phone").value = "";
+    document.getElementById("budget").value = "";
+    window.location = "thankyou.html";
+  });
+}
