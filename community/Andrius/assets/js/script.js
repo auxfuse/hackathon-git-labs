d0f31628bb1c5c1e656bfcf5ea33e0137e141ba8
andrius-siup <andrius.siupinys@gmail.com>
2022-10-20 12:07:53 +0100

    Removed unused code

--- community/Andrius/assets/js/script.js
+++ community/Andrius/assets/js/script.js
@@ -6,16 +6,13 @@ const modal = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
 
 about.addEventListener("click", function (e) {
-  // console.log(e.target.dataset.id);
   const id = e.target.dataset.id;
 
   if (id) {
-    // Remove active from other buttons
     btns.forEach(function (btn) {
       btn.classList.remove("active");
       e.target.classList.add("active");
     });
-    // hide other articles
     articles.forEach(function (article) {
       article.classList.remove("active");
     });
@@ -34,22 +31,18 @@ closeBtn.addEventListener("click", function () {
 
 function SendMail() {
   const params = {
-    // Send values
     from_name: document.getElementById("userName").value,
     email_id: document.getElementById("email_id").value,
     message: document.getElementById("message").value,
-    // new
     phone: document.getElementById("phone").value,
     budget: document.getElementById("budget").value,
   };
   emailjs.send("service_id", "template_id", params).then(function (res) {
-    // alert("Success! " + res.status);
     console.log("Success " + res.status);
-    // Then clear all values
+
     document.getElementById("userName").value = "";
     document.getElementById("email_id").value = "";
     document.getElementById("message").value = "";
-    // new
     document.getElementById("phone").value = "";
     document.getElementById("budget").value = "";
     window.location = "thankyou.html";
